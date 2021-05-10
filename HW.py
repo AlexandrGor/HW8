70ffa356be067a002e3b2890a0ba087879849c27
Alexandr Gorbishev <alexandrgorbishev@inbox.ru>
2021-05-10 19:01:44 +0300

    Task 2

--- HW.py
+++ HW.py
@@ -1,4 +1,5 @@
 import requests
+import pathlib
 class Super_Hero:
     url = ' https://superheroapi.com/api/'
     access_token = '2619421814940190'
@@ -20,9 +21,30 @@ def most_intelligence(heros):
             hero.get_intelligence()
     sorted_list_of_intelligence = sorted(heros, key=lambda hero: hero.intelligence)
     return sorted_list_of_intelligence[0]
+
+class YaUploader:
+    token = ''
+    def __init__(self, file_path):
+        self.file_path = file_path
+    def upload(self):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': 'OAuth {}'.format(self.token)}
+        params = {'path': 'disk:/Netology/' + self.file_path.name,
+                  'overwrite': 'true'}
+        upload_link = requests.get(url, headers=headers, params=params).json()['href']
+        res = requests.put(upload_link, data=open(self.file_path, 'rb'))
+        res.raise_for_status()
+        if res.status_code == 201:
+            return 'Файл успешно загружен на Я.Диск'
+        return 'Ошибка загрузки'
+
 if __name__ == "__main__":
     Super_Heros = [Super_Hero('Hulk'),
                    Super_Hero('Captain America'),
                    Super_Hero('Thanos')]
     winner = most_intelligence(Super_Heros)
     print(f"Самый умный - {winner.name}, его интеллект равен {winner.intelligence}")
+
+    uploader = YaUploader(pathlib.Path('files for yadisk', 'file1.txt'))
+    print(uploader.upload())
